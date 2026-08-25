8331dded08cc7af0764caa83920c98f942efa954
devankitx <ankitmaurya.coder@gmail.com>
2026-08-25 14:08:27 +0530

    mutable vs immutable borrowing

--- bootcamp-1/src/main.rs
+++ bootcamp-1/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let mut str = String::from("Hello");
-    update_str(&mut str);
-    println!("{}", str)
+    let mut s1 = String::from("Hello");
+    let s2 = &mut s1;
+    // update_word(&mut s1);
+    println!("{}", s1);
+    println!("{}", s2);
 }
 
-fn update_str(str: &mut String) {
-    str.push_str(" Ankit");
-}
+// fn update_word(word: &mut String) {
+//     word.push_str(" World");
+// }
