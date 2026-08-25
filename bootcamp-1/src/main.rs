aac066af503a70e90aab568ee6bea616c766cdab
devankitx <ankitmaurya.coder@gmail.com>
2026-08-25 10:15:39 +0530

    reference and borrowing

--- bootcamp-1/src/main.rs
+++ bootcamp-1/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut str = String::from("Hello");
+    update_str(&mut str);
+    println!("{}", str)
+}
+
+fn update_str(str: &mut String) {
+    str.push_str(" Ankit");
 }
