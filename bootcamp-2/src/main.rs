7a3412bcf099e4928b152afa49bf89479f5b1e4f
devankitx <ankitmaurya.coder@gmail.com>
2026-09-01 11:35:53 +0530

    rust bootacmp part 2

--- bootcamp-2/src/main.rs
+++ bootcamp-2/src/main.rs
@@ -2,7 +2,7 @@ fn get_string_len(str: String) -> usize {
     str.chars().count()
 }
 fn main() {
-    let name = String::from("Ankit Maurya");
+    let name = String::from("Ankit Mauryax   ");
     let len = get_string_len(name);
     println!("The len of the string is:- {}", len)
 }
