a1a92ca9a94f530adf5d4a192ea3a7859d998b51
devankitx <ankitmaurya.coder@gmail.com>
2026-09-22 17:38:22 +0530

    structs with enum

--- rust_solana/src/main.rs
+++ rust_solana/src/main.rs
@@ -1,10 +1,29 @@
-fn main() {
-    let s = String::from("Ankittrewytgre4tre");
-    println!("{}", is_longer_than(s, 10));
+struct User {
+    name: String,
+    age: i32,
+    gender: Gender,
+}
+enum Gender {
+    Male,
+    Female,
 }
 
-fn is_longer_than(s: String, num: usize) -> bool {
-    if s.len() > num {
+fn main() {
+    let user1 = User {
+        name: String::from("Ankit"),
+        age: 19,
+        gender: Gender::Male,
+    };
+    let user2 = User {
+        name: String::from("ankit2"),
+        age: 17,
+        gender: Gender::Female,
+    };
+    println!("{}", is_allowed_voitng(user1));
+    println!("{}", is_allowed_voitng(user2));
+}
+fn is_allowed_voitng(u: User) -> bool {
+    if u.age >= 18 {
         return true;
     } else {
         return false;
