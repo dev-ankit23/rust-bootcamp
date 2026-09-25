11c1baa56c1117584bac81b40b7a18985f87cddb
devankitx <ankitmaurya.coder@gmail.com>
2026-09-25 12:39:31 +0530

    generics

--- rust_solana/src/main.rs
+++ rust_solana/src/main.rs
@@ -1,16 +1,13 @@
-fn find_first_a(s: String) -> Option<i32> {
-    for (index, character) in s.chars().enumerate() {
-        if character == 'a' {
-            return Some(index as i32);
-        }
-    }
-    return None;
+ 
+pub fn main() {
+    let v = vec![1, 2, 3];
+    let v2 = vec![String::from("Harkirat"), String::from("Singh")];
+    let v3 = vec![1.0, 2.0, 3.0];
+    println!("{}", first_element(v).unwrap());
+    println!("{}", first_element(v2).unwrap());
+    println!("{}", first_element(v3).unwrap());
 }
 
-fn main() {
-    let my_string = String::from("hello");
-    match find_first_a(my_string) {
-        Some(index) => println!("The letter 'a' is found at index: {}", index),
-        None => println!("The letter 'a' is not found in the string."),
-    }
+fn first_element<T>(v: Vec<T>) -> Option<T> {
+    return v.into_iter().nth(0);
 }
